136ce50a7e0267ac9c2293275882b0610a4c4311
Valeria Rybakova <valerian.rybakova@yandex.ru>
2023-09-07 00:43:14 +0300

    fix: поправить атрибуты инпутов

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,24 +1,34 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import useForm from '../../hooks/useForm';
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
+import Preloader from '../Preloader/Preloader';
 import './SearchForm.css';
 
-function SearchForm() {
+function SearchForm({form, handleChange, searchMovies}) {
+
+  function handleSubmit (e) {
+    e.preventDefault();
+    searchMovies(form.keyword, form.checkbox);
+  }
+
   return (
     <section className='search-form'>
       <div className='search-form__content-box content-box'>
         <div className='search-form__icon' />
-        <form className='search-form__form' name='search'>
+        <form className='search-form__form' name='search' onSubmit={handleSubmit} noValidate>
           <fieldset className='search-form__field search-form__field_type_search'>
             <input 
               className='search-form__input'
               type='text'
-              name='search'
+              name='keyword'
+              value={form.keyword}
               placeholder='Фильм'
               id='search'
+              onChange={handleChange}
             />
             <button className='search-form__button button' type='submit' />
           </fieldset>
-            <FilterCheckbox />
+            <FilterCheckbox onChange={handleChange} value={form.checkbox} />
         </form>
       </div>
     </section>
