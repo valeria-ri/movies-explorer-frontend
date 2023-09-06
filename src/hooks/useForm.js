b357b1bc129b4857256689b233ecedf8157ef292
Valeria Rybakova <valerian.rybakova@yandex.ru>
2023-09-06 23:19:34 +0300

    feat: создать хук обработки и валидации формы

---
+++ src/hooks/useForm.js
@@ -0,0 +1,36 @@
+import { useState, useCallback } from 'react';
+
+const useForm = (initialState) => {
+  const [form, setForm] = useState(initialState);
+  const [errors, setErrors] = useState({});
+  const [isValid, setIsValid] = useState(false);
+  
+  const handleChange = (e) => {
+    const input = e.target;
+    
+    setErrors({
+      ...errors,
+      [input.name]: input.validationMessage,
+    })
+
+    setForm({
+      ...form,
+      [input.name]: input.value,
+    });
+
+    setIsValid(e.target.closest('form').checkValidity());
+  };
+
+  const resetForm = useCallback(
+    (newForm = {}, newErrors = {}, newIsValid = false) => {
+      setForm(newForm);
+      setErrors(newErrors);
+      setIsValid(newIsValid);
+    },
+    [setForm, setErrors, setIsValid]
+  );
+
+  return { form, errors, handleChange, isValid, resetForm };
+}
+
+export default useForm;
