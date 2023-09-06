fa62d96aa50af346abb5707a0b97fb3854be4123
Valeria Rybakova <valerian.rybakova@yandex.ru>
2023-09-07 01:04:09 +0300

    feat: реализовать вывод найденных карточек

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,8 +1,7 @@
-import React from 'react';
 import { useLocation } from 'react-router-dom';
 import './MoviesCard.css';
 
-function MoviesCard({image, nameRU, duration, isSaved}) {
+function MoviesCard({movie, isSaved}) {
   const location = useLocation();
   const saveBtnClassName = `movies-card__save-btn ${isSaved ? 'movies-card__save-btn_active' : ''}`;
   const deleteBtnClassName = 'movies-card__delete-btn';
@@ -16,12 +15,12 @@ function MoviesCard({image, nameRU, duration, isSaved}) {
     <li className='movies-card'>
       <div className='movies-card__info'>
         <div className='movies-card__description'>
-          <h2 className='movies-card__movie-name'>{nameRU}</h2>
-          <p className='movies-card__duration'>{duration}</p>
+          <h2 className='movies-card__movie-name'>{movie.nameRU}</h2>
+          <p className='movies-card__duration'>{movie.duration}</p>
         </div>
         <button className={btnClassName} type='button' />
       </div>
-      <img className='movies-card__image' src={image} alt={nameRU} />
+      <img className='movies-card__image' src={movie.image} alt={movie.nameRU} />
     </li>
   )
 }
--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,40 +1,26 @@
-import React from 'react';
 import { useLocation } from 'react-router-dom';
 import MoviesCard from '../MoviesCard/MoviesCard';
 import './MoviesCardList.css';
 
-// временное решение для визуализации вёрстки до подключения API
-import movies from '../../utils/movies';
-
-function MoviesCardList() {
+function MoviesCardList({filteredMovies}) {
   const location = useLocation();
-  const savedMovies = movies.filter(movie => movie.saved);
 
   return (
     <section className='movies-card-list'>
       <ul className='movies-card-list__grid'>
         {
-          (location.pathname === '/movies') ? movies.map((movie, i) => (
+          filteredMovies.map((movie) => (
           <MoviesCard 
-            key={i} 
+            key={movie._id || movie.movieId}
+            movie={movie}
             image={movie.image}
             nameRU={movie.nameRU}
             duration={movie.duration}
-            isSaved={movie.saved} 
           />
-          )) :
-          (location.pathname === '/saved-movies') && savedMovies.map((movie, i) => (
-            <MoviesCard 
-              key={i} 
-              image={movie.image}
-              nameRU={movie.nameRU}
-              duration={movie.duration}
-              isSaved={movie.saved} 
-            />
           ))
         }
       </ul>
-      {(location.pathname === '/movies') && <button className='movies-card-list__button button' type='button'>Ещё</button>}
+      {/* {(location.pathname === '/movies') && <button className='movies-card-list__button button' type='button'>Ещё</button>} */}
     </section>
   )
 }
