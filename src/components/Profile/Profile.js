821b4f1933e609e0dcccfc7f66384eb60fdb3b17
Valeria Rybakova <valerian.rybakova@yandex.ru>
2023-09-06 23:32:30 +0300

    feat: реализовать изменение данных пользователя

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,17 +1,69 @@
-import React, { useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import { CurrentUserContext } from '../../contexts/CurrentUserContext';
+import useForm from '../../hooks/useForm';
 import './Profile.css';
 
-function Profile({name, email}) {
+function Profile({handleUpdateUser, logOut}) {
   const [isEdited, setIsEdited] = useState(false);
-  const [isCorrect, setIsCorrect] = useState(true);
+  // const [isCorrect, setIsCorrect] = useState(true);
+  const [serverMessage, setServerMessage] = useState('');
+  const [serverMessageSuccessMode, setServerMessageSuccessMode] = useState(false);
 
-  const errorClassName = `profile__error${isCorrect ? '_hidden' : ''}`;
-  const submitClassName = `profile__submit ${!isCorrect ? 'profile__submit_disabled' : ''} button`;
+  const currentUser = useContext(CurrentUserContext);
+
+  const { form, errors, handleChange, isValid, resetForm } = useForm({
+    email: currentUser.email,
+    name: currentUser.name,
+  });
+
+  useEffect(() => {
+    resetForm({
+      email: currentUser.email,
+      name: currentUser.name,
+    })
+  }, [currentUser]);
+
+  function handleSubmit (e) {
+    e.preventDefault();
+    handleUpdateUser(form)
+      .then(() => {
+        setIsEdited(false);
+        setServerMessage('Данные обновлены')
+        // Можно добавить стейт для успешного сообщения, который управляет новым классом
+        setServerMessageSuccessMode(true);
+      })
+      .catch((errorCode) => {
+        // setIsCorrect(false);
+        setServerMessageSuccessMode(false);
+        if (errorCode === 400) {
+          return setServerMessage('Данные некорректны')
+        }
+        if (errorCode === 409) {
+          return setServerMessage('Пользователь с данным email уже существует')
+        }
+        setServerMessage('На сервере произошла ошибка')
+      });
+  }
+
+  function handleEditMode() {
+    setServerMessage('');
+    setIsEdited(true);
+  }
+
+  // function getErrorClassName (name) {
+  //   return `profile__error${errors[name] ? '_hidden' : ''}`
+  // }
+
+  function getErrorClassName (name) {
+    return `profile__input-error ${errors[name] ? 'profile__input-error_visible' : ''}`
+  }
+
+ const serverMessageClassName = `profile__server-message ${serverMessageSuccessMode ? 'profile__server-message_success' : ''}`;
 
   return (
     <main className='profile content content-box'>
-      <h1 className='profile__welcome'>Привет, {name}!</h1>
-      <form className='profile__form' name='profile'>
+      <h1 className='profile__welcome'>Привет, {currentUser.name}!</h1>
+      <form className='profile__form' name='profile' onSubmit={handleSubmit}>
         <fieldset className='profile__field'>
           <label className='profile__input-title'>Имя</label>
           <input
@@ -19,11 +71,15 @@ function Profile({name, email}) {
             type='text'
             name='name'
             placeholder='Имя'
-            defaultValue={name}
+            value={form.name || ''}
             id='name'
+            onChange={handleChange}
             disabled={!isEdited}
+            minLength='2'
+            maxLength='30'
           />
         </fieldset>
+        <span className={getErrorClassName('name')}>{errors.name}</span>
         <fieldset className='profile__field'>
           <label className='profile__input-title'>E-mail</label>
           <input
@@ -31,20 +87,22 @@ function Profile({name, email}) {
             type='email'
             name='email'
             placeholder='E-mail'
-            defaultValue={email}
+            value={form.email || ''}
             id='email'
+            onChange={handleChange}
             disabled={!isEdited}
           />
         </fieldset>
+        <span className={getErrorClassName('email')}>{errors.email}</span>
         {isEdited ? 
           <div className='profile__actions profile__actions_type_edit'>
-            <span className={errorClassName}>При обновлении профиля произошла ошибка.</span>
-            <button className={submitClassName} type='submit'>Сохранить</button>
+            <span className={serverMessageClassName}>{serverMessage}</span>
+            <button className='profile__submit button' type='submit' disabled={!isValid}>Сохранить</button>
           </div>
           :
           <div className='profile__actions profile__actions_type_profile'>
-            <button className='profile__button profile__button_type_edit button' type='button'>Редактировать</button>
-            <button className='profile__button profile__button_type_logout button' type='button'>Выйти из аккаунта</button>
+            <button className='profile__button profile__button_type_edit button' type='button' onClick={handleEditMode}>Редактировать</button>
+            <button className='profile__button profile__button_type_logout button' type='button' onClick={logOut}>Выйти из аккаунта</button>
           </div>
         }
       </form>
