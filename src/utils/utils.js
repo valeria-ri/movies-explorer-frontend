3d2928f877346d2093a0de9a6a7886e15a68c928
Valeria Rybakova <valerian.rybakova@yandex.ru>
2023-09-06 23:42:47 +0300

    feat: форматирование объекта и фильтра фильмов

---
+++ src/utils/utils.js
@@ -0,0 +1,43 @@
+import { MOVIES_URL } from "./constants";
+
+function moviesFormat(movies) {
+  return movies.map((movie) => {
+    return {
+      country: movie.country,
+      director: movie.director,
+      duration: movie.duration,
+      year: movie.year,
+      description: movie.description,
+      image: `${MOVIES_URL}${movie.image.url}`,
+      trailerLink: movie.trailerLink,
+      thumbnail: `${MOVIES_URL}${movie.image.formats.thumbnail.url}`,
+      movieId: movie.id,
+      nameRU: movie.nameRU,
+      nameEN: movie.nameEN,
+    }
+  })
+}
+
+function filterMovies(movies, keyword, isShort) {
+  const checkInclude = (item) => {
+    return item.toLowerCase().includes(keyword.toLowerCase())
+  }
+
+  const filteredByKeywordMovies = movies.filter((movie) => {
+    return checkInclude(movie.nameRU) || checkInclude(movie.nameEN);
+  })
+  
+  if (isShort) {
+    const shortMovies = filteredByKeywordMovies.filter((movie) => {
+      return movie.duration <= 40;
+    })
+    return shortMovies;
+  } else {
+    return filteredByKeywordMovies;
+  }
+}
+
+export {
+  moviesFormat,
+  filterMovies
+};
