f894f29ba1c877b9b20cf009f9c596cb41ead550
Valeria Rybakova <valerian.rybakova@yandex.ru>
2023-09-07 00:40:26 +0300

    feat: реализовать функцию получения фильмов

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import { useState } from 'react';
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
-// import Preloader from '../Preloader/Preloader';
+import { mainApi } from '../../utils/MainApi';
+import { moviesApi } from '../../utils/MoviesApi';
+import { moviesFormat, filterMovies } from '../../utils/utils';
+import Preloader from '../Preloader/Preloader';
+import MoviePage from '../MoviePage/MoviePage';
 
 function Movies() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  function getMovies() {
+    setIsLoading(true);
+    moviesApi
+      .getMovies()
+      .then((movies) => {
+        setMovies(moviesFormat(movies));
+        localStorage.setItem('movies', JSON.stringify(movies));
+      })
+      .catch(console.error)
+      .finally(() => setIsLoading(false))
+  }
+
   return (
-    <main className='content'>
-      <SearchForm />
-      {/* <Preloader /> */}
-      <MoviesCardList />
-    </main>
+    <MoviePage getMovies={getMovies} movies={movies} />
   )
 }
 
