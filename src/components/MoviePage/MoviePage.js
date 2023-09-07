9d635b75e839ae66376a624375610fda7f991505
Valeria Rybakova <valerian.rybakova@yandex.ru>
2023-09-07 03:27:42 +0300

    feat: восстановление предыдущего поиска

--- src/components/MoviePage/MoviePage.js
+++ src/components/MoviePage/MoviePage.js
@@ -3,26 +3,17 @@ import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import { filterMovies } from '../../utils/utils';
 import useForm from '../../hooks/useForm';
+import { useLocation } from 'react-router-dom';
 
 function MoviePage({ getMovies, movies }) {
-  const [filteredMovies, setFilteredMovies] = useState([]);
+  const location = useLocation().pathname;
 
-  const { form, handleChange, resetForm } = useForm({
-    keyword: '',
-    checkbox: false,
-  });
+  const [filteredMovies, setFilteredMovies] = useState(restorePrevSearch().filteredMovies);
 
-  // useEffect(() => {
-  //   resetForm({
-  //     keyword: '',
-  //     checkbox: false,
-  //   })
-  // }, [keyword]);
+  const { form, handleChange } = useForm(restorePrevSearch().form);
 
   useEffect(() => {
-    if (movies.length === 0 && getMovies) {
-      return;
-    }
+    if (movies.length === 0 && getMovies) return;
     setFilteredMovies(filterMovies(movies, form.keyword, form.checkbox));
   }, [movies, form.checkbox]);
 
@@ -31,9 +22,36 @@ function MoviePage({ getMovies, movies }) {
     if (movies.length === 0 && getMovies) {
       getMovies();
     } else {
-      setFilteredMovies(filterMovies(movies, keyword, isShort));
+      setFilteredMovies(filterMovies(movies, keyword, isShort));      
     }
   }
+
+  useEffect(() => {
+    if (location !== '/movies') return;
+    localStorage.setItem('prevSearch', JSON.stringify({
+      filteredMovies,
+      form,
+    }));
+  }, [filteredMovies, form.checkbox]);
+
+  function restorePrevSearch() {
+    if (location !== '/movies') return {
+      filteredMovies: [],
+      form: {
+        keyword: '',
+        checkbox: false,
+      },
+    };
+    const prevSearch = JSON.parse(localStorage.getItem('prevSearch'));
+    if (!prevSearch) return {
+      filteredMovies: [],
+      form: {
+        keyword: '',
+        checkbox: false,
+      },
+    };
+    return prevSearch;
+  };
 
   return (
     <main className='content'>
--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -8,16 +8,21 @@ import Preloader from '../Preloader/Preloader';
 import MoviePage from '../MoviePage/MoviePage';
 
 function Movies() {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState(restoreMovies());
   const [isLoading, setIsLoading] = useState(false);
+
+  function restoreMovies() {
+    return JSON.parse(localStorage.getItem('movies') ?? '[]');
+  }
 
   function getMovies() {
     setIsLoading(true);
     moviesApi
       .getMovies()
-      .then((movies) => {
-        setMovies(moviesFormat(movies));
-        localStorage.setItem('movies', JSON.stringify(movies));
+      .then((newMovies) => {
+        const formattedMovies = moviesFormat(newMovies)
+        setMovies(formattedMovies);
+        localStorage.setItem('movies', JSON.stringify(formattedMovies));
       })
       .catch(console.error)
       .finally(() => setIsLoading(false))
