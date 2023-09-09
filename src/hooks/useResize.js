8f7ccd22140a1537341e10d85a571f8d61d0abd0
Валерия <valeriari@MacBook-Pro-Valeria.local>
2023-09-09 23:49:21 +0300

    fix: увеличить задержку в хуке

--- src/hooks/useResize.js
+++ src/hooks/useResize.js
@@ -4,12 +4,12 @@ function useResize() {
     // стейт ширины окна браузера
     const [width, setWidth] = useState(window.innerWidth);
 
-    // подписка на изменение ширины окна с задержкой на полсекунды
+    // подписка на изменение ширины окна с задержкой на секунду
     useEffect(function() {
         function handleResize(e) {
             setTimeout(function() {
                 setWidth(e.target.innerWidth);
-            }, 500)
+            }, 1000)
         }
         window.addEventListener('resize', handleResize);
         return function() {
